00132b521e6aa03e0444a041a6c385ac10bb69ce
unknown <sufyanali800@gmail.com>
2017-09-06 03:38:07 -0700

    react first app

---
+++ src/StateAndStatelessComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// class App extends React.Component{
+   // render(){
+	   // return React.createElement('h1',null,'Hello React JS!')  
+   // }
+// }
+
+const App = () => <h1>Hello ReactJS....</h1>
+ 
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import App from './App';
+import App from './StateAndStatelessComponent';
+ReactDOM.render(
+		<App />,
+		document.getElementById("root")  
+);
